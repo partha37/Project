3d64f273182c0d7a43c702a26e3113ae674f73f6
partha37 <parthi37cse@gmail.com>
2020-09-24 14:33:35 +0530

    Add files via upload

---
+++ Taskpro/JavaScript/dist/Form.dev.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var movies_list = {
+  'movies': [{
+    'Name': 'THE SMURFS',
+    'Data': ['1 October, Sunday - 9:00 AM, AUDI - 1', '1 October, Sunday - 2:00 PM, AUDI - 1', '1 October, Sunday - 8:00 PM, AUDI - 1', '2 October, Sunday - 9:00 AM, AUDI - 1', '2 October, Sunday - 2:00 PM, AUDI - 1', '2 October, Sunday - 8:00 PM, AUDI - 1']
+  }, {
+    'Name': 'MINIONS',
+    'Data': ['1 October, Sunday - 9:00 AM, AUDI - 2', '1 October, Sunday - 2:00 PM, AUDI - 2', '1 October, Sunday - 8:00 PM, AUDI - 2', '2 October, Sunday - 9:00 AM, AUDI - 2', '2 October, Sunday - 2:00 PM, AUDI - 2', '2 October, Sunday - 8:00 PM, AUDI - 2']
+  }, {
+    'Name': 'INSIDE OUT',
+    'Data': ['1 October, Sunday - 9:00 AM, AUDI - 3', '1 October, Sunday - 2:00 PM, AUDI - 3', '1 October, Sunday - 8:00 PM, AUDI - 3', '2 October, Sunday - 9:00 AM, AUDI - 3', '2 October, Sunday - 2:00 PM, AUDI - 3', '2 October, Sunday - 8:00 PM, AUDI - 3']
+  }]
+};
+var forms = document.getElementsByClassName('form');
+
+if (forms != null) {
+  var i,
+      j,
+      value = 0;
+
+  for (i = 0; i < 3; i++) {
+    var movie_name = forms[0].getElementsByTagName('legend')[i];
+    var data = forms[0].getElementsByTagName('select')[i];
+    movie_name.innerHTML = movies_list.movies[i].Name;
+
+    for (j = 0; j < movies_list.movies[i].Data.length; j++) {
+      var opt = document.createElement('option');
+      opt.value = value++;
+      opt.name = 'dataIndex';
+      opt.innerHTML = movies_list.movies[i].Data[j];
+      data.appendChild(opt);
+    }
+  }
+}
